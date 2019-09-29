5cdb77a9e746d1c6d32c579ff4edfd0206008122
Valeria38 <valeriia308@gmail.com>
2019-09-29 21:53:41 +0300

    Updates

---
+++ precache-manifest.d9f517db14e44ce94f329df5c03210c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2891a6e935c4adcc9f0c5c71d3aa33",
+    "url": "/user-settings-editor/index.html"
+  },
+  {
+    "revision": "70a003fbbfba2fba1b7f",
+    "url": "/user-settings-editor/static/css/2.cc82b09a.chunk.css"
+  },
+  {
+    "revision": "0e0023b6861d3e757fed",
+    "url": "/user-settings-editor/static/css/main.d06e50b6.chunk.css"
+  },
+  {
+    "revision": "70a003fbbfba2fba1b7f",
+    "url": "/user-settings-editor/static/js/2.3c8e703c.chunk.js"
+  },
+  {
+    "revision": "0e0023b6861d3e757fed",
+    "url": "/user-settings-editor/static/js/main.3276ae62.chunk.js"
+  },
+  {
+    "revision": "0d1ff0088e2f89425e2d",
+    "url": "/user-settings-editor/static/js/runtime-main.afff51ea.js"
+  }
+]);
